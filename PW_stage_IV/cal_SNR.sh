dddabbd6a0a79874e4296eddc45c55de41e5bc16
zdplayground <zd585612@ohio.edu>
2018-11-29 14:14:20 -0500

    update

--- PW_stage_IV/cal_SNR.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for nbin_case in $(seq 0 2); do
-  echo "nbin_case: $nbin_case"
-  mpirun -n 4 ./PW_SN_ratio_Takada_Jain.py --snf 1.0 --nbin_case $nbin_case --Pk_type Pwig_nonlinear --comm_size 4 --idir0 ./ --odir0 ./
-done
