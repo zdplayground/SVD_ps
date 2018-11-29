8fc64bb0e2d11047fc308ff64a9cf785567284ac
zdplayground <zd585612@ohio.edu>
2018-11-29 14:23:09 -0500

    first commit

---
+++ KW_stage_IV/run_code.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+nkout=66
+snf=1.0
+#snf=0.2558
+
+alpha=1.0
+idir0_Cijl="./BAO_alpha_"$alpha/
+idir0_Gm="./BAO_alpha_"$alpha/
+odir0=$idir0_Cijl
+
+cal_cijl="False"
+cal_Gm="False"
+cal_sn="False"
+for nrbin in 30; do
+    echo $nrbin
+    time mpirun -n 4 ./cal_Cijl_Gm.py --nrbin $nrbin --nkout $nkout --Pk_type Pwig_nonlinear --Psm_type Pnorm --alpha $alpha --cal_sn $cal_sn --cal_cijl $cal_cijl --cal_Gm $cal_Gm --odir0 $odir0 --show_nz True
+#    time mpirun -n 4 ./cal_Cijl_Gm.py --nrbin $nrbin --nkout $nkout --Pk_type Pnow --Psm_type Pnorm --alpha $alpha --cal_sn True --cal_cijl $cal_cijl --cal_Gm False --odir0 $odir0
+#for snf in 1e-06; do
+    #mpirun -n 4 ./mpi_KW_sn_dep_Cov_cij_cross_bin.py --nrbin $nrbin --num_kout $nkout --snf $snf --Pk_type Pwig_nonlinear --idir0_Cijl $idir0_Cijl --idir0_Gm $idir0_Gm --odir0 $odir0
+    #mpirun -n 4 ./mpi_KW_sn_dep_Cov_cij_cross_bin.py --nrbin $nrbin --num_kout $nkout --snf $snf --Pk_type Pnow --idir0_Cijl $idir0_Cijl --idir0_Gm $idir0_Gm --odir0 $odir0
+#done
+done
+
+
+
+
+
+
