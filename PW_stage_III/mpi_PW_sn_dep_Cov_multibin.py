09359fbae219d56bf64b2f2817ca2ef14a8db558
zdplayground <zd585612@ohio.edu>
2018-11-29 13:44:24 -0500

    update

--- PW_stage_III/mpi_PW_sn_dep_Cov_multibin.py
+++ PW_stage_III/mpi_PW_sn_dep_Cov_multibin.py
@@ -1,21 +1,13 @@
 #!/Users/ding/anaconda3/bin/python
-# Copy the same code from folder ../PW_stage_IV, modify it for PW-Stage IV survey, i.e., f_sky=0.1212 for the condition of DES (as PW-Stage III). --03/09/2018
-# Need to set value of num_sbin and num_pbin correctly for large number of tomographic bins. -- 03/19/2018
-##############################################################################################################
-# Copy the code TW_sn_dep_Cov_Cij.py, rename it by adding _bin, means outputing .bin files. 07/23/2016 (Remain the previous notes.)
-# Change shape noise terms in Cov(C^ii(l), C^jj(l)), see how the recovered P(k) and error bars from
-# SVD change. Compare the effect on both cases:TF and TW.
-# Construct Goutprime
-##################################################################################################################
-# logout without change: 12/19/2015
-# fix a bug ell value: 12/30/2015
-# modified: 03/03/2016,  rename the file from shape_n.py to shape_noise_dependent_Cov_Cij.py
-# 04/17/2016, modify some variables and make the code especially for TW case
-# 04/25/2016, modify the whole main loop. Take care of C^ij(l) and Gm_cross_out which include external bins. We
-# select the first $red_bin bins to match with the Tully-Fisher case.
-# 08/21/2017, modify it to be Python3 version.
-# 10/22/2017, it's better to output data into separate files from each process. Because it's faster than collectively writing data into one
-# single file (7 times faster with 4 processes running). Also it doesn't introduce systematics in the matrix inversion process.
+# Copy the code from ../PW_stage_IV, modify f_sky,
+# -------------------------------------------------------------------
+# 1. It's better to output data into separate files from each process. Because it's faster than collectively writing data into one
+# single file (7 times faster with 4 processes running). Also it doesn't introduce systematics in the matrix inversion process. --10/22/2017
+# 2. Modify reading in Cijl and G data for the case of G with 66 output k bins. -- 06/15/2018
+# 3. For generating G' with 66 output k bins, comment out the output of Cijl'. -- 06/16/2018
+# 4. Output of Cijl' and G' with shape noise factor 0.1838. --07/06/2018
+# 5. Be careful about inputting Cijl and G data files; whether we read a single file or multiple files with rank id appended. --07/13/2018
+# 6. Add the additional parameter alpha denoting the BAO scale shifting parameter. It's useful to do Fisher analysis. --09/03/2018
 #
 from mpi4py import MPI
 import numpy as np
@@ -23,19 +15,18 @@ import math
 import os, sys
 from scipy import integrate
 from scipy import linalg
-sys.path.append('/Users/ding/Documents/playground/shear_ps/SVD_ps/TW_f2py_SVD/')
+sys.path.append('/Users/ding/Documents/playground/shear_ps/SVD_ps/common_modules/')
 from cov_matrix_module import cal_cov_matrix
 import argparse
 
-#---------- set parameters correspondingly ------------#
-#snf = 5.93e-5        # this matches the scale factor 1.e-3 in Tully-Fisher case
 #---------------------------------------------------------
-parser = argparse.ArgumentParser(description="Calculate G' with shape noise dependence in Cov(C^ii(l), C^jj(l)), made by Zhejie.")
+parser = argparse.ArgumentParser(description="Calculate Cijl' and G' with shape noise dependence in Cov(C^ii(l), C^jj(l)), made by Zhejie.", formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument("--snf", help = '*The shape noise factor from the default value.', required=True)
 parser.add_argument("--nbin_case", help = '*Case id for the number of tomographic bins. See the directory num_rbin in the code.', type=int, required=True)
 parser.add_argument("--num_kout", help = '*Number of output k bins.', type=int, required=True)
 parser.add_argument("--Pk_type", help = "*The type of input P(k), whether it's linear (Pwig), or damped (Pwig_nonlinear), or without BAO (Pnow).", required=True)
 parser.add_argument("--comm_size", help = '*The number of the processes used to generate preliminary data.', required=True)
+parser.add_argument("--alpha", help = 'alpha, the BAO scale shifting parameter.', default=1.0, type=np.float, required=True)
 
 args = parser.parse_args()
 snf = float(args.snf)
@@ -43,6 +34,7 @@ nbin_case = args.nbin_case
 num_kout = args.num_kout
 Pk_type = args.Pk_type
 comm_size = args.comm_size
+alpha = args.alpha
 
 #---------------------------------------------------------
 def cal_sn_dep_Cov_cij():
@@ -52,8 +44,8 @@ def cal_sn_dep_Cov_cij():
     comm.Barrier()
     t_start = MPI.Wtime()
 
-    #num_rbin = {'num_sbin': (5, 15, 30, 100, 150), 'num_pbin': (6, 19, 38, 127, 191)}    # s: spectroscopic; p: photometric -- This case is correct only for n(z) Stage-III.
-    num_rbin = {'num_sbin': (5, 30), 'num_pbin': (6, 38)}    # s: spectroscopic; p: photometric
+    num_rbin = {'num_sbin': (5, 30),
+                'num_pbin': (6, 38)}    # s: spectroscopic; p: photometric
 
     red_bin = num_rbin['num_sbin'][nbin_case]
     red_bin_ext = num_rbin['num_pbin'][nbin_case]
@@ -66,21 +58,24 @@ def cal_sn_dep_Cov_cij():
     #delta_l = 1
     delta_l = 3
     num_l = (l_max - l_min)//delta_l + 1
-    f_sky = 5000.0/41253.0            # Survey area is 5000 deg^2 in PW-Stage III (DES).
+    f_sky = 5000.0/41253.0            # Survey area 5000 deg^2 is for PW-Stage III (DES)
     #print("f_sky: ", f_sky)
     data_type_size = 8
 
     prefix = 'TW_zext_'
-    idir = './mpi_preliminary_data_{}/'.format(Pk_type, comm_size)
+    idir0 = './BAO_alpha_{}/'.format(alpha)
+    ##idir = './mpi_preliminary_data_{}/comm_size{}/'.format(Pk_type, comm_size)
+    idir = idir0 + 'mpi_preliminary_data_{}/'.format(Pk_type)
     #------------- !! write output files, they are the basic files --------------#
-    ofdir = './mpi_{}sn_exp_k_data_{}/comm_size{}/'.format(prefix, Pk_type, size)
+    ofdir = idir0 + 'mpi_{}sn_exp_k_data_{}/comm_size{}/'.format(prefix, Pk_type, size)
+    Gm_ifprefix = idir0 + 'mpi_preliminary_data_Pwig_nonlinear/' + prefix
     ofprefix = ofdir + prefix
     print('Output file prefix:', ofprefix)
     if rank == 0:
         if not os.path.exists(ofdir):
             os.makedirs(ofdir)
         # read shape noise term \sigma^2/n^i
-        inputf = './mpi_preliminary_data_Pwig_nonlinear/comm_size{}/'.format(comm_size) + prefix + 'pseudo_shapenoise_{0}rbins_ext.out'.format(red_bin_ext)
+        inputf = Gm_ifprefix + 'pseudo_shapenoise_{0}rbins_ext.out'.format(red_bin_ext)
         pseudo_sn_ext = np.loadtxt(inputf, dtype='f8', comments='#')
         pseudo_sn = np.array(pseudo_sn_ext[0: red_bin])*snf
         print(pseudo_sn.shape)
@@ -113,9 +108,15 @@ def cal_sn_dep_Cov_cij():
     #print('Cij(l) from rank', rank, 'is:', Cijl_sets, '\n')
     comm.Barrier()
     Cijl_freader.Close()
+    # Since Cijl has been generated with equal number of ells from different 4 ranks, we could directly read data from sub-binary files.
+    # file_Cijl_cross = idir + 'comm_size{}/'.format(comm.size) + prefix + 'Cij_l_{}rbins_ext_{}kbins_CAMB_rank{}.bin'.format(red_bin_ext, num_kin, rank) # Cij_l stores Cij for each ell by row
+    # Cijl_freader = open(file_Cijl_cross, 'rb') # Open and read a binary file
+    # Cijl_sets = np.fromfile(Cijl_freader, dtype='d', count=-1, sep='')
+    # #print('Cij(l) from rank', rank, 'is:', Cijl_sets, '\n')
+    # Cijl_freader.close()
 
     #--------------- !! read Gm_cross part by part for each ell -----------------#
-    file_Gm_cross = './mpi_preliminary_data_Pwig_nonlinear/' + prefix + 'Gm_cross_out_{}rbins_{}kbins_CAMB.bin'.format(red_bin_ext, num_kout)
+    file_Gm_cross = Gm_ifprefix + 'Gm_cross_out_{}rbins_{}kbins_CAMB.bin'.format(red_bin_ext, num_kout)
     Gm_freader = MPI.File.Open(comm, file_Gm_cross)
     Gm_fh_start = rank * Gm_len * data_type_size
     Gm_freader.Seek(Gm_fh_start)
@@ -135,22 +136,27 @@ def cal_sn_dep_Cov_cij():
         #print(cijl_array, cijl_array.shape)
         cijl_m[iu1] = np.array(cijl_array)
         #print(cijl_m, cijl_m.shape)
-        cijl_m_select = np.array(cijl_m[0:red_bin, 0:red_bin])   # select the first red_bin bins of Cij, match the case with T-F
+        cijl_m_select = np.array(cijl_m[0:red_bin, 0:red_bin])     # select the first red_bin bins of Cij, match the case with T-F
         cijl_true = np.array(cijl_m_select[iu2])                   # convert upper triangle matrix to an array
         cijl_sn = np.array(cijl_true)
         cijl_sn[sn_id] = cijl_true[sn_id] + pseudo_sn              # add shape noise terms in Cii(l) terms
 
-        Cov_cij_cpq = cal_cov_matrix(red_bin, iu2, cijl_sn)     # calculate the covariance matrix of Cij(l), Cpq(l')
+        Cov_cij_cpq = cal_cov_matrix(red_bin, iu2, cijl_sn)        # calculate the covariance matrix of Cij(l), Cpq(l')
+        # if rank == 0:
+        #     rank_matrix = np.linalg.matrix_rank(Cov_cij_cpq)
+        #     print('ell, rank of Cov:', ell, rank_matrix)
+
         Cov_cij_cpq = Cov_cij_cpq/((2.0*ell+1.0)*delta_l*f_sky)      # account the number of modes for each l with the interval delta_l
 
         w_ccij, v_ccij = linalg.eigh(Cov_cij_cpq, lower=False, overwrite_a=True)  # Get eigenvalue and eigenvectors from Scipy routine
         w_inv = 1.0/w_ccij
-
+        if not np.all(w_inv>0.0):
+            print('w_inv from ell ', ell, ' is negative.')        # show below which ell, the inverse of Cov_cij_cpq fails
         # If uncomment the below, overwrite_a should be set False in the linalg.eigh()
-        #sqrt_w_inv = np.diag(w_inv**0.5)
-        #v_inv = np.transpose(v_ccij)
-        #Cov_cij_sym = np.triu(Cov_cij_cpq, k=1) + Cov_cij_cpq.T
-        #print reduce(np.dot, [np.diag(sqrt_w_inv**2.0), v_inv, Cov_cij_sym, v_inv.T])
+        # sqrt_w_inv = np.diag(w_inv**0.5)
+        # v_inv = np.transpose(v_ccij)
+        # Cov_cij_sym = np.triu(Cov_cij_cpq, k=1) + Cov_cij_cpq.T
+        # print reduce(np.dot, [np.diag(sqrt_w_inv**2.0), v_inv, Cov_cij_sym, v_inv.T])
 
         Cov_inv_half = np.transpose(w_inv**0.5 *v_ccij)               # Simplify the expression of dot(sqrt_w_inv, v_inv), 05/09/2016
         G_l_array = Gm_sets[l*N_dset_ext*num_kout: (l+1)*N_dset_ext*num_kout]
--- PW_stage_III/run_code.sh
+++ PW_stage_III/run_code.sh
@@ -1,16 +1,26 @@
 #!/bin/bash -l
-
 nkout=66
-snf=3.1
+#nb_case_id=(4 6 8 9 10)
+nrbin=30       # modify the nbin_case correspondingly
+#snf=1.0
+snf=0.3226    # match the effective shape noise with that of PWL-Stage IV  
 
-#for nrbin in 5; do
-##mpirun -n 4 ./f2py_TW_zextend_multibin.py --nrbin $nrbin --nkout $nkout --Pk_type Pwig_nonlinear --cal_sn True --cal_cijl True --cal_Gm True
-##mpirun -n 4 ./f2py_TW_zextend_multibin.py --nrbin $nrbin --nkout $nkout --Pk_type Pnow --cal_cijl True
-#
-#mpirun -n 4 ./mpi_PW_sn_dep_Cov_multibin.py --snf $snf --nbin_case 0 --num_kout $nkout --Pk_type Pwig_nonlinear --comm_size 4
-#mpirun -n 4 ./mpi_PW_sn_dep_Cov_multibin.py --snf $snf --nbin_case 0 --num_kout $nkout --Pk_type Pnow --comm_size 4
+cal_sn="False"
+cal_cijl="False"
+cal_Gm="False"
+
+#for nrbin in 30; do
+#echo $nrbin
+#for alpha in 1.0; do
+mpirun -n 4 ./cal_Cijl_Gm.py --nrbin $nrbin --nkout $nkout --Pk_type Pwig_nonlinear --cal_sn $cal_sn --cal_cijl $cal_cijl --cal_Gm $cal_Gm --odir0 ./ --alpha $alpha --show_nz True
+#mpirun -n 4 ./cal_Cijl_Gm.py --nrbin $nrbin --nkout $nkout --Pk_type Pnow --cal_sn False --cal_cijl True --cal_Gm False --odir0 ./ --alpha $alpha
+#done
 #done
 
-nrbin=30
-mpirun -n 4 ./f2py_TW_zextend_multibin.py --nrbin $nrbin --nkout $nkout --Pk_type Pwig_nonlinear --odir0 "./" --cal_sn True --cal_cijl True --alpha 1.002
-mpirun -n 4 ./f2py_TW_zextend_multibin.py --nrbin $nrbin --nkout $nkout --Pk_type Pnow --odir0 "./" --cal_cijl True --alpha 1.002
+alpha=1.0
+# be careful to set nbin_case correctly corresponding to nrbin value.
+for nrbin_case in 1; do
+mpirun -n 4 ./mpi_PW_sn_dep_Cov_multibin.py --snf $snf --nbin_case $nrbin_case --num_kout $nkout --Pk_type Pwig_nonlinear --comm_size 4 --alpha $alpha
+mpirun -n 4 ./mpi_PW_sn_dep_Cov_multibin.py --snf $snf --nbin_case $nrbin_case --num_kout $nkout --Pk_type Pnow --comm_size 4 --alpha $alpha
+done
+   
